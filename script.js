6e1773b5c4b0194efcf62697670393a6ab8a64dc
dfel08 <dfellows68@gmail.com>
2020-03-27 13:28:48 -0700

    added h4 tag to titles

--- script.js
+++ script.js
@@ -25,10 +25,13 @@ function getData() {
   var article;
   var description;
   for (let i = 0; i < requestedNumber; i++) {
-    var titleDiv = $("<div>");
-    titleDiv.text(NYTData.articles[i].title);
-    $("#appended-articles").append(titleDiv);
-    
+    var titleH4 = $("<h4>");
+    titleH4.text(NYTData.articles[i].title);
+    $("#appended-articles").append(titleH4);
+    var authorP = $("<p>")
+    authorP.text(NYTData.articles[i].author)
+    $("#appended-articles").append(authorP)
+
     NYTData.articles[i].source;
     NYTData.articles[i].title;
     NYTData.articles[i].author;
